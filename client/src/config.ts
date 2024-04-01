752502a0515bfb53e4eb7089b83e3f1d102822ac
Juho Kangas <juho.kangas@aalto.fi>
2024-04-01 10:53:04 +0300

    vite dotenv fix

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,9 @@
-const CLIENT_ID = "d9cad2b46b2f47fe8168ea6456212665"
-const REDIRECT_URI = "http://localhost:5173"
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-const RESPONSE_TYPE = "token"
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
+export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI
+export const AUTH_ENDPOINT = import.meta.env.VITE_AUTH_ENDPOINT
+export const RESPONSE_TYPE = import.meta.env.VITE_RESPONSE_TYPE
 
+export const STRAPI_TOKEN = import.meta.env.VITE_STRAPI_TOKEN
 export const STRAPI_URL = import.meta.env.VITE_STRAPI_URL ?? 'http://localhost:1337'
+
 export const VALIDATE_URL = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
--- client/src/strapi-api.ts
+++ client/src/strapi-api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
-
-const token = '0ff34ee27b44bbe62389370dd40728cb0d24119ef8b04353b41ca835c0babde62061037cfb7630175a17703cd98a898ead23aa14e18d34242aa38f5698c1568285d8ee776dfc95a8fe4d05a3ff04d10c35884207d3cc3f3778ec9ce0d2d967caf9dbe73b2bb22189ff209323aafcadf1c4530c2e6340dba0b7afd36f52c2b37a'
+import { STRAPI_TOKEN } from './config'
 
 export const getAllStrapiAlbums = async () => {
   const {data} = await axios.get(`http://localhost:1337/api/albums`, {
@@ -32,7 +31,7 @@ export const putAlbum = async (id: string, rating: number) => {
     url: 'http://localhost:1337/api/albums/',
     headers: { 
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer ${STRAPI_TOKEN}`
     },
     data : data
   }
